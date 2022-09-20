821fdc03856ca8047885f25b53c46d951dde8542
Mohammad Abtahi <smabtahi922gmail.com>
2022-09-20 15:45:02 +0430

    basement

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
-type clientType struct{
-	ID string 'json:"ID'
-	
+func rootHandler() {
+
 }
 
 func main() {
-
+	http.HandleFunc("/", rootHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }

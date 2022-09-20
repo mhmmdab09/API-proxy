6ffa5dee6e92827ea3d89e589152b799532c9812
Mohammad Abtahi <smabtahi922gmail.com>
2022-09-20 14:10:46 +0430

    f1

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welcome home!")
+}
 
 func main() {
-	fmt.Print("Halloha")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }

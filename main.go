e272bb7e9bf9274842e6f11049c5a6c6c0b9254b
Mohammad Abtahi <smabtahi922gmail.com>
2022-09-22 10:07:03 +0330

    second api call generated

--- main.go
+++ main.go
@@ -54,6 +54,35 @@ func callService(authKey string, authValue string, serviceID string, baseU strin
 }
 
 func main() {
+
+	var pointToAddressService anyService
+	pointToAddressService.ID = "01"
+	pointToAddressService.name = "Neshan API"
+	pointToAddressService.baseURL = "https://api.neshan.org/v5/reverse"
+	pointToAddressService.requestPATH = "?lat=32.654012&lng=51.666944"
+	pointToAddressService.secretKey = "Api-Key"
+	pointToAddressService.secretValue = "service.f586da437b9147999e42808212e4b573"
+
+	var distanceService anyService
+	distanceService.ID = "02"
+	distanceService.name = "Weather API"
+	distanceService.baseURL = "https://api.neshan.org/v1/distance-matrix"
+	distanceService.requestPATH = "?type=car&origins=36.3177579,59.5323219&destinations=36.35067,59.5451965"
+	distanceService.secretKey = "Api-Key"
+	distanceService.secretValue = "service.21db1d0baa3c42838f0cdf68a8c8073a"
+
+	callService(distanceService.secretKey,
+		distanceService.secretValue,
+		distanceService.ID,
+		distanceService.baseURL,
+		distanceService.requestPATH)
+
+	callService(pointToAddressService.secretKey,
+		pointToAddressService.secretValue,
+		pointToAddressService.ID,
+		pointToAddressService.baseURL,
+		pointToAddressService.requestPATH)
+
 	/*
 		http.HandleFunc("/", rootHandler)
 		err := http.ListenAndServe("localhost:11111", nil)
@@ -62,24 +91,4 @@ func main() {
 			os.Exit(1)
 		}
 	*/
-	var pointToAddressService anyService
-	pointToAddressService.ID = "01"
-	pointToAddressService.name = "Neshan API"
-	pointToAddressService.baseURL = "https://api.neshan.org/v5/reverse"
-	pointToAddressService.requestPATH = "?lat=32.654012&lng=51.666944"
-	pointToAddressService.secretKey = "Api-Key"
-	pointToAddressService.secretValue = "service.76e24479dd3f4e3b83d686a2b179d08d"
-
-	var weatherService anyService
-	weatherService.ID = "02"
-	weatherService.name = "Weather API"
-	weatherService.baseURL = ""
-	weatherService.secretKey = ""
-	weatherService.secretValue = ""
-
-	callService(pointToAddressService.secretKey,
-		pointToAddressService.secretValue,
-		pointToAddressService.ID,
-		pointToAddressService.baseURL,
-		pointToAddressService.requestPATH)
 }
